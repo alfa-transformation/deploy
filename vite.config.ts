29e562f059f8488170edec6e71c37d7a3067ea47
alfiya <alfiya>
2024-04-21 18:02:54 +0300

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/alfa-transformation.github.io/",
+  base: "/deploy/",
   server: {
     port: 3000,
   }
