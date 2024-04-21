2ef1aa6942eb07a2bdff0d403961e6498200ed2a
alfiya <alfiya>
2024-04-21 18:07:27 +0300

    small fix

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,8 +1,14 @@
-import { memo } from "react"
+import { memo, useState } from "react"
 import styles from './Header.module.scss'
 
 function Header() : JSX.Element {
-  return <p className={styles['test']}>Sorry for the inconvenience, the site is under construction</p>
+  const [open, setIsOpen] = useState(false)
+
+  return <p className={styles['test']}>Sorry for the inconvenience, the site is under construction
+  <button onClick={() => setIsOpen(true)}> click here </button>
+
+  {open && <p>Я не думал, что вы сюда кликните</p>}
+  </p>
 }
 
 export default memo(Header);
